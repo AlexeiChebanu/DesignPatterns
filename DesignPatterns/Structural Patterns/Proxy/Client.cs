4b5b990178648949b9c50a24becc97d3081d2b06
Alexei <0alexchebanu@gmail.com>
2023-08-15 09:56:28 +0300

    Proxy

---
+++ DesignPatterns/Structural Patterns/Proxy/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural_Patterns.Proxy;
+
+public class Client
+{
+    public void ClientCode(ISubject subject)
+    {
+        subject.Request();
+    }
+}
+
+class ProgramProxy
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        Console.WriteLine("Client: Executing the client code with a real subject:");
+        RealSubject realSubject = new RealSubject();
+        client.ClientCode(realSubject);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Client: Executing the same client code with a proxy:");
+        Proxy proxy = new Proxy(realSubject);
+        client.ClientCode(proxy);
+    }
+}
---
+++ DesignPatterns/Structural Patterns/Proxy/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Structural_Patterns.Proxy;
+
+public interface ISubject
+{
+    void Request();
+}
+
+class RealSubject : ISubject
+{
+    public void Request()
+    {
+        Console.WriteLine("RealSubject: Handling Request.");
+    }
+}
